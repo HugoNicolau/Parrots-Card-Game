da24b7fa5625733ebbaaaf86569d94ff8c1afe89
Hugo <nicolau.hugogiles@gmail.com>
2022-08-22 13:09:51 -0300

    Básico do jogo e do virar de cartas funcionando

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let qtdcartas = Number(prompt(
-  "Com quantas cartas quer jogar?(Apenas números pares de 4 a 14)"
-));
+let qtdcartas = Number(
+  prompt("Com quantas cartas quer jogar?(Apenas números pares de 4 a 14)")
+);
 while (
   qtdcartas != 4 &&
   qtdcartas != 6 &&
@@ -13,6 +13,9 @@ while (
     "Com quantas cartas quer jogar?(Apenas números pares de 4 a 14"
   );
 }
+
+let cartasViradas = 0;
+let arrViradas = [];
 
 let arrayCartas = [
   "./img/1",
@@ -30,7 +33,6 @@ let arrayCartas = [
   "./img/7",
   "./img/7",
 ];
-
 
 let embaralhar = arrayCartas.slice(0, qtdcartas); // Após esta linha, a minhaArray estará embaralhada
 embaralhar.sort(comparador);
@@ -59,18 +61,33 @@ function porCartas() {
     //colocar todas as cartas diferentes
   }
 }
-function esconderFrente(element){
-    element.classList.add('virada')
-
+function esconderFrente(element) {
+  if (cartasViradas < 2) {
+    arrViradas.push(element);
+    element.classList.add("virada");
+    cartasViradas++;
+    //se forem 2 viradas, comparar pra ver se são pares e, se forem, adicionar ao array e não desvirar, se não forem, contar um tempo e desvirar
+    if (cartasViradas == 2) {
+        console.log(arrViradas[arrViradas.length-1].children[1].innerHTML)
+      if (arrViradas[arrViradas.length-1].children[1].innerHTML !=arrViradas[arrViradas.length-2].children[1].innerHTML) {
+        console.log(arrViradas[arrViradas.length-1].children[1])
+        setTimeout(virarDeVolta, 1000, arrViradas);
+        cartasViradas-=2
+      }
+      else {
+        cartasViradas-=2
+        
+      }
+    }
+  } else {
+    //não virar nem adicionar ao array de viradas
+  }
+  //Virar se tiver menos que duas viradas
 }
 
-
-
-function virar() {
-  let card = document.querySelector(".carta");
-  let frontcard = document.querySelector(".carta .frente-carta");
-  let backcard = document.querySelector(".carta .costas-carta");
-  console.log(card);
-  card.classList.toggle(".virar");
-  frontcard.classList.toggle(".escondido");
+function virarDeVolta(arrayV) {
+  let a = arrayV.length;
+  arrayV[a - 1].classList.remove("virada");
+  arrayV[a - 2].classList.remove("virada");
+  //element.classList.remove('virada')
 }
